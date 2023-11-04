b278bffcd3fa3f5ce176dae5b4df1a01ff168f09
Kenny Pflug <kenny.pflug@thinktecture.com>
2023-11-04 10:30:23 +0100

    feat: transaction is no longer optional in NpgsqlSession

--- 2023-11-03_data-access-in-native-aot/WebApp/DatabaseAccess/NpgsqlSession.cs
+++ 2023-11-03_data-access-in-native-aot/WebApp/DatabaseAccess/NpgsqlSession.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
 using Npgsql;
@@ -6,12 +7,14 @@ using Npgsql;
 namespace WebApp.DatabaseAccess;
 
 public abstract class NpgsqlSession(NpgsqlConnection connection,
-                                    IsolationLevel? transactionLevel = IsolationLevel.ReadCommitted)
+                                    IsolationLevel transactionLevel = IsolationLevel.ReadCommitted)
     : ReadOnlyNpgsqlSession(connection, transactionLevel), ISession
 {
     public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         if (Transaction is not null)
             await Transaction.CommitAsync(cancellationToken);
+        else
+            throw new InvalidOperationException("The transaction was not initialized beforehand");
     }
 }
